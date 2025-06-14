2907dba07e6933a5b5d73fc26da86192b01154da
Lekchem Barro <lekchembarro.devs@gmail.com>
2025-06-14 17:39:13 +0800

    Initial commit

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "0a4206d4ce3eec2e6edec6a543a29993";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
